64f782a10a0f8b5851bc7d85088e4135f28e38b1
Sivnarayan Pal <shibu.bapoon100@gmail.com>
2022-01-23 11:49:22 +0530

    more frag connected to cube frag

--- app/src/main/java/com/example/everycalc/Mensuration3D.java
+++ app/src/main/java/com/example/everycalc/Mensuration3D.java
@@ -2,25 +2,35 @@ package com.example.everycalc;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavGraph;
 import androidx.navigation.NavInflater;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.card.MaterialCardView;
+
 
 public class Mensuration3D extends Fragment {
-
+    MaterialCardView cardViewCube;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_mensuration3_d, container, false);
-
+        cardViewCube=view.findViewById(R.id.cubeMen3D);
+        cardViewCube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_mensuration3D_to_cubeFragment);
+            }
+        });
 
     return view;
     }
